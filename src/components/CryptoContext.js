b906fdb5614fd871e106ebf98198dde8093e4795
Prince <prince.mishra@spit.ac.in>
2022-10-31 18:26:14 +0530

    Initial commit

---
+++ src/components/CryptoContext.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { createContext } from 'react'
+const Crypto=createContext()
+
+const CryptoContext = () => {
+  return (
+  <Crypto.Provider>
+    
+  </Crypto.Provider>
+  )
+}
+
+export default CryptoContext
